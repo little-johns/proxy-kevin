40023790851e8e6aeb5a8c17ffb1da3d2ccb25c5
Kevin Yang <kkaiyang94@gmail.com>
2019-04-30 14:49:37 -0700

    Imeplement for local use

--- Proxy/server.js
+++ Proxy/server.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -6,7 +7,7 @@ const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(morgan('dev'));
 app.use(cors());
 app.use('/:id', express.static(path.join(__dirname, './public')));
 
@@ -17,20 +18,26 @@ app.use('/:id', express.static(path.join(__dirname, './public')));
   app.get('/api/:stockId', (req, res) => {
     chartReq.get(`api/${req.params.stockId}`)
     .then((response) => {
-      res.send(response.data);
+      res.status(200).send(response.data);
+    })
+    .catch(error => {
+      console.log(error);
     })
   })
 
-  const buyFormReq = axios.create({
-      baseURL: 'http://ec2-3-84-115-167.compute-1.amazonaws.com:8080/' 
-  });
+  // const buyFormReq = axios.create({
+  //     baseURL: 'http://ec2-3-84-115-167.compute-1.amazonaws.com:8080/' 
+  // });
 
-  app.get('/stocks/:query', (req, res) => {
-      buyFormReq.get(`stocks/${req.params.query}`)
-      .then((response) => {
-          res.send(response.data);
-      })
-  })
+  // app.get('/stocks/:query', (req, res) => {
+  //     buyFormReq.get(`stocks/${req.params.query}`)
+  //     .then((response) => {
+  //         res.status(response.data);
+  //     })
+  //     .catch(() => {
+  //       res.status(400);
+  //     })
+  // })
 
 app.listen(port, () => {
     console.log('Server is listening on port', port);
